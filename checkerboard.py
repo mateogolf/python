7a20efc85509c986423876a86875db190f362d3d
mateogolf <rodriguezml11@mail.wlu.edu>
2017-07-31 16:20:03 -0400

    Checkerboard with one loop printing each line

---
+++ checkerboard.py
@@ -0,0 +1,10 @@
+def checkerboard():
+    # string = ""
+    for i in range(1,9):
+        if i % 2 == 0:
+            print "* * * * "
+        else
+            print " * * * *"
+
+    #for odd levels, start with *
+    #for even levels, start with " "
